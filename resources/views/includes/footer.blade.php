e97e70fba513ff8ed62238adddc218b8bcffc3ff
Mykyta <mp091689@localhost.localdomain>
2016-06-20 13:25:13 +0300

    footer small changes

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -4,7 +4,7 @@
             СТО «На Оболони»<br>
             адрес: г. Киев, ул. Полярная, д. 19<br>
             Гаражный кооператив "Припять"<br>
-            Бокс 335<br>
+            Бокс 335<br><br>
             <a href="mailto:obolon.sto@gmail.com">obolon.sto@gmail.com</a><br>
             телефон: +38 (050) 500-91-11<br>
             телефон: +38 (097) 839-95-54
@@ -12,12 +12,12 @@
         <section class="footer-social">
             <p>Мы в социальных сетях</p>
             <a href="#" class="social-btn social-btn-vk">Вконтакте</a>
-            <a href="#" class="social-btn social-btn-fb">Фейсбук</a>
+            <a href="https://www.facebook.com/sto.obolon/" class="social-btn social-btn-fb">Фейсбук</a>
             {{--<a href="#" class="social-btn social-btn-inst">Инстаграм</a>--}}
         </section>
         <section class="footer-copyright">
             <p>Автор сайта:</p>
-            <a href="mailto:mp091689@gmail.com?subject=Автор сайта СТО на оболони" class="btn" style="text-transform: none">Mykyta P.</a>
+            <a href="https://github.com/mp091689" class="btn" style="text-transform: none">Mykyta P.</a>
         </section>
     </div>
 </footer>
