5fb71fe1cadf9e037ff78d157606bce497e8b124
mp091689 <mp091689@gmail.com>
2016-06-28 15:33:13 +0300

    checking2

--- resources/views/pages/public.blade.php
+++ resources/views/pages/public.blade.php
@@ -19,7 +19,7 @@
         <div class="logo">
             <img src="src/img/logo.png" alt="STO-Obolon">
             <span style="color: #fff; font-size: 48px; font-style: italic">СТО "НА ОБОЛОНИ"</span><br/>
-            <span style="color: #fccf1b; font-size: 22px; font-style: italic">Качественный сервис для вашей машины!</span>
+            <span style="color: #fccf1b; font-size: 22px; font-style: italic">Качественный сервис для вашей машины</span>
         </div>
 
         <section class="features-row clearfix">
@@ -32,7 +32,7 @@
                 <p>В СТО на оболони работают професионалы своего дела, мы гарантируем качественное выполнение технических работ.</p>
             </div>
             <div class="features-item">
-                <div class="features-title">Надежно</div>
+                <div class="features-title">Доступно</div>
                 <p>Ваш автомобиль прослужит вам еще долго без сбоев и неполадок.</p>
             </div>
         </section>
