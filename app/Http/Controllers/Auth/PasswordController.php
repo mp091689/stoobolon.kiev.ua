b029e6f1dc7dea0c528085b8234de0fe49c3704c
mp091689 <mp091689@gmail.com>
2016-07-09 12:22:30 +0300

    password reset is fixed

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -19,7 +19,7 @@ class PasswordController extends Controller
     */
 
     use ResetsPasswords;
-
+    protected $redirectTo = '/admin';
     /**
      * Create a new password controller instance.
      *
